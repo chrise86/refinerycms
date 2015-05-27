3ffcdd728900ccd10e9005c75633d21e6dd0158b
Chris Edwards <chris@chrise.net>
2015-05-27 20:22:16 +0100

    custom js

--- core/app/assets/javascripts/admin.js
+++ core/app/assets/javascripts/admin.js
@@ -5,3 +5,51 @@
 if (typeof(custom_visual_editor_options) == "undefined") {
   custom_visual_editor_options = {};
 }
+
+var custom_visual_editor_boot_options = {
+  containersItems: [
+    {'name': 'h1', 'title':'Heading_1', 'css':'wym_containers_h1'}
+    , {'name': 'h2', 'title':'Heading_2', 'css':'wym_containers_h2'}
+    , {'name': 'h3', 'title':'Heading_3', 'css':'wym_containers_h3'}
+    , {'name': 'h4', 'title':'Heading_4', 'css':'wym_containers_h4'}
+    , {'name': 'h5', 'title':'Heading_5', 'css':'wym_containers_h5'}
+    , {'name': 'p', 'title':'Paragraph', 'css':'wym_containers_p'}
+  ],
+  classesItems: [
+    {
+      name: 'text-align',
+      rules: [
+        {name: 'left', title: '{Left}'},
+        {name: 'center', title: '{Center}'},
+        {name: 'right', title: '{Right}'},
+        {name: 'justify', title: '{Justify}'}
+      ],
+      join: '-',
+      title: '{Text_Align}'
+    }, {
+      name: 'image-align',
+      rules: [
+        {name: 'left', title: '{Left}'},
+        {name: 'right', title: '{Right}'}
+      ],
+      join: '-',
+      title: '{Image_Align}'
+    }, {
+      name: 'font-size',
+      rules: [
+        {name: 'small', title: '{Small}'},
+        {name: 'normal', title: '{Normal}'},
+        {name: 'large', title: '{Large}'}
+      ],
+      join: '-',
+      title: '{Font_Size}'
+    }, {
+      name: 'custom',
+      rules: [
+        {name: 'lead', title: 'Lead'}
+      ],
+      join: '-',
+      title: 'Classes'
+    }
+  ]
+};
